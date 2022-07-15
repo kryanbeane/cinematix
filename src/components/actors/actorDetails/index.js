2aea6f16e9ba544372928f56c9e80bb85707fe8c
Bryan Keane <bkeane@redhat.com>
2022-07-15 12:22:08 +0100

    Moved actor and movie summaries to details

--- src/components/actors/actorDetails/index.js
+++ src/components/actors/actorDetails/index.js
@@ -1,5 +1,10 @@
 import React from "react";
-import {Chip, Typography, makeStyles, Paper} from "@material-ui/core";
+import {Grid, Chip, Typography, makeStyles, Paper} from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import img from "../../../images/actor-poster-placeholder.jpg";
+import CardContent from "@material-ui/core/CardContent";
+import TextInfoContent from '@mui-treasury/components/content/textInfo';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,30 +25,77 @@ const useStyles = makeStyles((theme) => ({
     },
     boldText: {
         fontWeight: "bold"
-    }
+    },
+    summary: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+    },
 }));
 
 const ActorDetails = ({ actor }) => {
     const classes = useStyles();
     return (
         <>
-            <Typography variant="h5" component="h3" className={classes.boldText}>
-                Biography
-            </Typography>
-            <br/>
-            <Typography variant="h6" component="p">
-                {actor.biography}
-            </Typography>
-            <br/>
-            <Typography variant="h5" component="h3" className={classes.boldText}>
-                Movie Credits
-            </Typography>
+            <Grid item xs={3}>
+                <div className={classes.summary}>
+                    <Card>
+                        <CardMedia>
+                            <img
+                                width={"100%"}
+                                src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`}
+                                alt={img}
+                            />
+                        </CardMedia>
+                        <CardContent>
+                            <TextInfoContent
+                                heading={"Known For"}
+                                body={actor.known_for_department}
+                            />
+                            <TextInfoContent
+                                heading={"Gender"}
+                                body={
+                                    actor.gender === 1 ? "Female"
+                                        : actor.gender === 2 ? "Male"
+                                            : "Other"
+                                }
+                            />
+                            <TextInfoContent
+                                heading={"Birthdate"}
+                                body={actor.birthday}
+                            />
+                            <TextInfoContent
+                                heading={"Place of Birth"}
+                                body={actor.place_of_birth}
+                            />
+                            <TextInfoContent
+                                heading={"Also Known As"}
+                                body={actor.also_known_as.map((item) => item).join(', ')}
+                            />
+                        </CardContent>
+                    </Card>
+                </div>
+            </Grid>
 
-            <Paper component="ul" className={classes.root}>
-                <li>
-                    <Chip label="TODO" className={classes.chip} color="primary" />
-                </li>
-            </Paper>
+            <Grid item xs={9}>
+                <Typography variant="h5" component="h3" className={classes.boldText}>
+                    Biography
+                </Typography>
+                <br/>
+                <Typography variant="h6" component="p">
+                    {actor.biography}
+                </Typography>
+                <br/>
+                <Typography variant="h5" component="h3" className={classes.boldText}>
+                    Movie Credits
+                </Typography>
+
+                <Paper component="ul" className={classes.root}>
+                    <li>
+                        <Chip label="TODO" className={classes.chip} color="primary" />
+                    </li>
+                </Paper>
+            </Grid>
         </>
     );
 };
